12fba505e77c702ed80e52659845a5a42a462806
Albertperlas <66008580+Albertperlas@users.noreply.github.com>
2023-05-30 10:13:06 +0200

    Update dna_pipeline.sh

--- dna_pipeline.sh
+++ dna_pipeline.sh
@@ -10,8 +10,6 @@
 # Read command-line arguments
 input_fast5_dir=$1
 output_fastq_file=$2
-reference_genome_file=$3
-output_sam_file=$4
 
 # Base calling with Guppy
 ./ont-guppy/bin/guppy_basecaller \
@@ -20,12 +18,3 @@ output_sam_file=$4
     --detect_barcodes \
     -c dna_r10.4.1_e8.2_400bps_hac.cfg \
     -x "cuda:0"
-
-
-# Mapping with Minimap2 from direct RNA-seq data
-minimap2 \
-    -ax splice \
-    -uf -k14 \
-    "$reference_genome_file" \
-    "$output_fastq_file" \
-    -o "$output_sam_file"
